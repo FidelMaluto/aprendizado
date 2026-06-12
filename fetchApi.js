39d43eb6e06d28cdcb201d1d71569da8b53f705b
Fidel Maulto <fidel.maluto77@gmail.com>
2026-06-12 07:21:34 +0100

    Refactor: Melhorando os comentários

--- fetchApi.js
+++ fetchApi.js
@@ -1,10 +1,13 @@
-//CONSUMINDO RECURSOS COM fetch();
+// CONSUMINDO RECURSOS COM fetch();
 const url = 'http://localhost:3003/carros';
 
 async function chamarApi() {
     const resp = await fetch(url);
-    //convertendo a respos em JSON
+
+    // Verificando o status
     if(resp.status === 200){
+
+    // convertendo a respos em JSON
         const obj = await resp.json();
 
         console.log(obj)
