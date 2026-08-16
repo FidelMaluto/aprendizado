3e72a9552f2a804fefc322294cfef0e1f4245a77
Fidel Maulto <fidel.maluto77@gmail.com>
2026-08-16 22:12:11 +0100

    fix: Resolvendo o problemazinho de enviar sms vazia

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ function cadastra() {
     const text = document.getElementById('text');
     const task = text.value.trim();
 
+    if (text.value == '') return;
+
     const li = document.createElement('li');
     li.id = 'li';
     li.textContent = task;
