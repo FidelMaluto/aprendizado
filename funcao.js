7cc90fbbaf0d25511c575f29efaa0090fb7211fb
Fidel Maulto <fidel.maluto77@gmail.com>
2026-08-20 22:21:44 +0100

    feat: Criando uma função dentro da class!

--- funcao.js
+++ funcao.js
@@ -3,7 +3,12 @@ class Pessoa {
         this.nome = nome,
         this.sobrenome = sobrenome
     }
+
+    nomeCompleto() {
+        console.log(this.nome + ' ' + this.sobrenome);
+    }
+    
 }
 
 const p1 = new Pessoa("Fidel", "Lweji");
-console.log(p1);
+console.log(p1.nomeCompleto());
