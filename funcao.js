483a41dbb950556a5b7431a40664a9ebdea9e5d5
Fidel Maulto <fidel.maluto77@gmail.com>
2026-08-21 23:17:48 +0100

    feat: Criando as funções!

--- funcao.js
+++ funcao.js
@@ -3,9 +3,16 @@ class Cao {
         this.nome = nome;
         this.raca = raca;
     }
+    
+    latir () {
+        return `${this.nome} está latindo...`;
+    }
 
+    correr () {
+        return `${this.nome} está correndo!`;
+    }
 
 }
 
 const p1 = new Cao('Máx', 'Rafeiro');
-console.log(p1);
+console.log(p1.latir());
